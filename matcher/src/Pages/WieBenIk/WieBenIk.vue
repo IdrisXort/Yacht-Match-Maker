1b6a892b511e8c53c6e37548adfbbdb1568280ed
IdrisXort <idris.genc@yacht.nl>
2019-01-08 14:01:15 +0100

    New Text  box added for Self info about user

--- matcher/src/Pages/WieBenIk/WieBenIk.vue
+++ matcher/src/Pages/WieBenIk/WieBenIk.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-      <br>
-    <text-input-field/><br>
-    <image-input-field/><br>
-    <hobies/><br>
+    <br>
+    <text-input-field whatToChange="name" label="What is je naam? "/>
+    <br>
+    <text-input-field
+      whatToChange="selfInfo"
+      label="Hoe zou je jezelf willen introduceren in één zin? "
+    />
+    <br>
+    <image-input-field/>
+    <br>
+    <hobies/>
+    <br>
   </div>
 </template>
 <script>
 import TextInputField from "../../Components/TextInputField/TextInputField";
 import ImageInputField from "./../../Components/ImageInputField/ImageInputField";
-import  Hobies  from "../../Components/Hobies/Hobies";
+import Hobies from "../../Components/Hobies/Hobies";
 export default {
   components: {
     TextInputField: TextInputField,
     ImageInputField: ImageInputField,
-    Hobies:Hobies
+    Hobies: Hobies
   },
   data() {
-    return {
-    };
+    return {};
   }
 };
 </script>
