e9e213ad6c91e80ddc51422f5495f8f17c74ec01
IdrisXort <idris.genc@yacht.nl>
2019-01-08 11:29:08 +0100

    Refactored BreadCrumb that it returns only one Crumb, no methods

--- matcher/src/Components/BreadCrumbs/BreadCrumb.vue
+++ matcher/src/Components/BreadCrumbs/BreadCrumb.vue
@@ -1,39 +1,37 @@
 <template>
   <div class="BreadCrumb-container">
-    <button class="circle"> <span  >1</span></button><hr>
-    <button class="circle"> <span  >2</span></button><hr>
-    <button class="circle"> <span  >3</span></button><hr>
-    <button class="circle"> <span  >4</span></button><hr>
-    <button class="circle"> <span  >5</span></button>
-
+    <button class="circle">{{pageNumber}}</button>
+    <hr>
   </div>
 </template>
 
 <script>
+import { EventBus } from "../../Event-bus";
 export default {
   data() {
-    return;
-  }
+    return {
+      pageNumber: 0
+    };
+  },
+  props: ["pageNumber"]
 };
 </script>
 <style>
-.circle{
-    display: flex;
-    align-items: center;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    text-align: center;
-    margin: auto;
+.circle {
+  display: flex;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  text-align: center;
+  margin: auto;
 }
-hr{
-    width: 100px;
+hr {
+  width: 100px;
 }
-.BreadCrumb-container{
-
-    display: flex;
-    align-items: center;
+.BreadCrumb-container {
+  display: flex;
+  align-items: center;
 }
-
 </style>
 
