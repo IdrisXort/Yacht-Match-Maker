539abb3feb35e3e58c095c2d0052ef20b8449b95
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:26:08 +0100

    ImageInputField Toegevoegd

---
+++ matcher/src/Components/ImageInputField/ImageInputField.vue
@@ -0,0 +1,24 @@
+<template>
+<input type="file"
+       accept="image/png, image/jpeg"
+       @change="getImage"
+       >
+</template>
+<script>
+import { EventBus } from '../../Event-bus';
+export default {
+    data(){
+        return{
+            image:null
+
+        }
+    },
+  methods: {
+      getImage(event){
+          this.image=event.target.value;
+
+          EventBus.$emit('imageTaken',this.image);
+      }
+  }
+};
+</script>
