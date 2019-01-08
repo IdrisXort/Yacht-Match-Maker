afcd46ba0a6be30e1ccaa7a6367f24769d15d3b4
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:27:19 +0100

    Logo Toegevoegd

---
+++ matcher/src/Components/Logo/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <p>YACHT </p><p>Match Finder</p>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+h1,h3{
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+}
+</style>
