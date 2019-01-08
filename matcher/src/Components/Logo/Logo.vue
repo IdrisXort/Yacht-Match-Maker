50d77de32fddbbc160e9a20a7f4484e75ed5784c
Adjay <adjayramkhelawan@hotmail.com>
2019-01-08 15:17:42 +0100

    Remade logo with css (and changed class from breadcrumb)

--- matcher/src/Components/Logo/Logo.vue
+++ matcher/src/Components/Logo/Logo.vue
@@ -1,16 +1,32 @@
 <template>
-<div>
-    <p>YACHT </p><p>Match Finder</p>
-</div>
+    <div>
+        <span class="logo">YACHT</span>
+        <span class="sublogo">Match finder</span>
+    </div>
 
 </template>
 <script>
-export default {
-    
-}
+    export default {
+
+    }
 </script>
 <style scoped>
-h1,h3{
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-}
+    h1,
+    h3 {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+
+    .logo {
+        font-family: Helvetica;
+        font-size: 35pt;
+        font-weight: 700;
+        letter-spacing: 2px;
+        margin-right: 10px;
+    }
+
+    .sublogo {
+        font-family: Helvetica;
+        font-size: 23pt;
+        color: #0f1941;
+    }
 </style>
