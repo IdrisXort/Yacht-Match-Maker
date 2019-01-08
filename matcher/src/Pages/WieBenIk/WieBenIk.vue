fbba71558c838e3b57b3464331e64eb1f72329f3
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:27:07 +0100

    Wie ben ik pagina Toegevoegd

---
+++ matcher/src/Pages/WieBenIk/WieBenIk.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <br>
+    <text-input-field/><br>
+    <image-input-field/><br>
+    <hobies/><br>
+  </div>
+</template>
+<script>
+import TextInputField from "../../Components/TextInputField/TextInputField";
+import ImageInputField from "./../../Components/ImageInputField/ImageInputField";
+import  Hobies  from "../../Components/Hobies/Hobies";
+export default {
+  components: {
+    TextInputField: TextInputField,
+    ImageInputField: ImageInputField,
+    Hobies:Hobies
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
