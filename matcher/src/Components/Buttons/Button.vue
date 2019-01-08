55fa74f76aea700754d8f2cbdb09d74ac9a73228
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:25:12 +0100

    Button Toegevoegd

---
+++ matcher/src/Components/Buttons/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="btn" @click="onClick">{{text}}</button>
+</template>
+<script>
+export default {
+  props: ["onClick", "text"],
+  data() {
+    return {};
+  }
+};
+</script>
