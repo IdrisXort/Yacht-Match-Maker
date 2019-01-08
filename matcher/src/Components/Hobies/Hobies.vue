1c916e46d818bb79f3c09545c814aad228b88950
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:26:31 +0100

    Hobies Toegevoegd

---
+++ matcher/src/Components/Hobies/Hobies.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div>
+    <input type="checkbox" @click="getHobby" name="hobby" value="sporten">
+    <label for="hobby">sporten</label>
+    <input type="checkbox" @click="getHobby" name="hobby" value="muziek">
+    <label for="hobby">muziek</label>
+    <input type="checkbox" @click="getHobby" name="hobby" value="fotograferen">
+    <label for="hobby">fotograferen</label>
+    <input type="checkbox" @click="getHobby" name="hobby" value="koken">
+    <label for="hobby">koken</label>
+    <input type="checkbox" @click="getHobby" name="hobby" value="tekenen">
+    <label for="hobby">tekenen</label>
+    <input type="checkbox" @click="getHobby" name="hobby" value="lezen">
+    <label for="hobby">lezen</label>
+  </div>
+</template>
+
+<script>
+import { EventBus } from "../../Event-bus";
+export default {
+  data() {
+    return {
+      hobies: []
+    };
+  },
+  methods: {
+    getHobby(event) {
+      if (event.target.checked) {
+        this.hobies.push(event.target.value);
+      }else{
+          this.hobies=this.hobies.filter(a=>a!=event.target.value)
+      }
+       EventBus.$emit('hobyChanged',this.hobies);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
