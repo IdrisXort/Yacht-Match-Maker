6f5c1709a9f8d5a964680143a3649651e5f1ffc6
IdrisXort <idris.genc@yacht.nl>
2019-01-08 10:25:42 +0100

    TextImageField toegevoegd

---
+++ matcher/src/Components/TextInputField/TextInputField.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" @change="changeName">
+</template>
+<script>
+import { EventBus } from '../../Event-bus';
+export default {
+    data(){
+        return{
+            name:''
+        }
+    },
+  methods: {
+      changeName(event){
+          this.name=event.target.value;
+          EventBus.$emit('nameChanged',this.name);
+      }
+  }
+};
+</script>
