3eed90c91527ec32ff255baa95563e62e82bb067
Gilary <Gilary_FB@live.nl>
2019-01-08 16:18:12 +0100

    added button style and font awesome

--- matcher/src/Components/Buttons/Button.vue
+++ matcher/src/Components/Buttons/Button.vue
@@ -1,16 +1,51 @@
 <template>
-  <button class="btn button" @click="onClick">{{text}}</button>
+  <button :class="className" @click="onClick">
+    <i v-show="previousIcon" class="fas fa-caret-left icon__left"></i>
+    {{text}}
+    <i v-show="nextIcon" class="fas fa-caret-right icon__right"></i>
+  </button>
 </template>
 <script>
 export default {
   props: ["onClick", "text"],
   data() {
-    return {};
+    return {
+      className:'btn button__'+this.text,
+      previousIcon:this.text == 'previous',
+      nextIcon:this.text == 'next',
+    };
   }
 };
 </script>
-<style>
-  .button {
-    background-color: green;
+<style scoped>
+  .btn {
+    color:white;
+    border:none;
+    border-radius:6px;
+    padding: 10px 20px;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 25px;
+    background-color: #192a56;
   } 
+
+  .button__start {
+    background-color: #e84118;
+  }
+
+  .icon__right {
+    font-size: 14px;
+    margin-left: .5em;
+  }
+
+  .icon__left{
+    font-size: 14px;
+    margin-right: .5em;
+  }
+
+  button:hover {
+   background: linear-gradient(100deg,#192a56,#e84118);
+   /* we zien wel wat de hover wordt voor nu even gradient*/
+}
+
 </style>
