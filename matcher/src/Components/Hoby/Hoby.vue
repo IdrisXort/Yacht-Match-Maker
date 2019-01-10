6a5bacd8ee0376a89c2b343a25fcdb402a10f712
IdrisXort <idris.genc@yacht.nl>
2019-01-10 22:24:54 +0100

    Added Hoby Component

---
+++ matcher/src/Components/Hoby/Hoby.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <Label>{{hoby}}</Label>
+    <input type="checkbox" @click="getHobby" name="hobby" :value="hoby">
+  </li>
+</template>
+
+<script>
+export default {
+    props:['hoby','getHobby']
+};
+</script>
+
+<style>
+</style>
