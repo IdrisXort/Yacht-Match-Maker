52aae28300fd12e222215c27bcee2278c5f47c14
Gilary <Gilary_FB@live.nl>
2019-01-10 15:17:28 +0100

    changed image input field style

--- matcher/src/Components/ImageInputField/ImageInputField.vue
+++ matcher/src/Components/ImageInputField/ImageInputField.vue
@@ -1,7 +1,10 @@
 <template>
 <div>
 <Label>{{label}}</Label>
-  <input type="file" accept="image/png, image/jpeg" @change="getImage">
+  <label class="image__field--button">
+    <i class="fas fa-upload icon__left"></i>upload
+    <input type="file" accept="image/png, image/jpeg" @change="getImage">
+  </label>
 </div>
 </template>
 <script>
@@ -22,3 +25,24 @@ export default {
   }
 };
 </script>
+<style>
+  input[type="file"] {
+    display: none;
+  }
+  .image__field--button {
+    color:white;
+    border:none;
+    border-radius:6px;
+    padding: 10px 20px;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 25px;
+    background-color: #192a56;
+  } 
+
+  .icon__left{
+    font-size: 14px;
+    margin-right: .5em;
+  }
+</style>
+
