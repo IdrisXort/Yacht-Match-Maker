9ffe9cd110ff23b9faccdad71dad5b313abcd72c
Gilary <Gilary_FB@live.nl>
2019-01-10 13:52:57 +0100

    added styling to input field

--- matcher/src/Components/TextInputField/TextInputField.vue
+++ matcher/src/Components/TextInputField/TextInputField.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-<Label>{{label}}</Label>
-  <input type="text" :whatToChange="whatToChange" @change="changeName">
+<Label class="label">{{label}}</Label>
+  <input type="text" :whatToChange="whatToChange" @change="changeName" class="input__field input__fold">
 </div>
 </template>
 <script>
@@ -24,3 +24,26 @@ export default {
   }
 };
 </script>
+<style>
+  .label{
+    width: 100%;
+    display: inline-block;
+  }
+ 
+  .input__field {
+    color:black;
+    border: 2px solid #192a56;
+    border-radius:5px;
+    width: 50%; /* moet nog bepaald worden watvoor width het krijgt en als het 50% is dat het op mobiel/tablet 100% is*/
+    padding: 5px;
+    margin-top: 10px;
+    font-size: 14px;
+    line-height: 20px;
+    }
+
+  .input__field:focus {
+    border-color: #192a56;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(3, 5, 83, 0.6);
+
+  }
+</style>
