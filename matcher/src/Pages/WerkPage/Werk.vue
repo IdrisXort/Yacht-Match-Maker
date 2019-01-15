ddb5561f08269e21a74d3186753b05c1449e7053
Adjay <adjayramkhelawan@hotmail.com>
2019-01-15 15:13:08 +0100

    Added Slider as component again in Werk.vue

--- matcher/src/Pages/WerkPage/Werk.vue
+++ matcher/src/Pages/WerkPage/Werk.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         dit is werk paginaa
+        <Slider></Slider>
         <Map :locations="locations" label="In welke regio's zou je het liefst willen werken?"/>
         <hard-skills :skills="skills" label="Kies je Hard Skills" />
     </div>
@@ -8,10 +9,12 @@
 <script>
 import Map from "../../Components/Map/Map";
 import HardSkills from '../../Components/HardSkills/HardSkills';
+import Slider from '../../Components/Slider/Slider'
 export default {
     components: {
     Map: Map,
-    HardSkills:HardSkills
+    HardSkills:HardSkills,
+    Slider: Slider
     },
     data(){
         return{}
