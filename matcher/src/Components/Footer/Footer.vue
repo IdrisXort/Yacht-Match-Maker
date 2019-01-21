388c66395f0e92e28a2651dbca006b1d35f158f1
Gilary <Gilary_FB@live.nl>
2019-01-21 16:02:25 +0100

    Changed footer styling

--- matcher/src/Components/Footer/Footer.vue
+++ matcher/src/Components/Footer/Footer.vue
@@ -10,46 +10,24 @@
     }
 </script>
 <style scoped>
-    h1,
-    h3 {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    }
-
-    /* .logo-container {
-        cursor: pointer;
-    } */
-
-    .logo {
-        font-family: Helvetica;
-        font-size: 30pt;/* font-size: 35pt; */
-        font-weight: 700;
-        letter-spacing: 2px;
-        margin-right: 10px;
-    }
-
-    .sublogo {
-        font-family: Helvetica;
-        font-size: 20pt;/* font-size: 23pt; */
-        color: #0f1941;
-    } 
     nav{
 			/* -webkit-box-shadow: 0 -3px -20px 0 rgba(0,0,0,.1);
 			-moz-box-shadow: 0 -3px -20px 0 rgba(0,0,0,.1);
 			box-shadow: 0 -3px -20px 0 rgba(0,0,0,.1); */
-			background-color:white;
-			-webkit-box-shadow: 0px -5px 20px rgba(0, 0, 0, 0.1);
-			-moz-box-shadow: 0px -5px 20px rgba(0, 0, 0, 0.1);
-			box-shadow: 0px -5px 20px rgba(0, 50, 0, 0.1);
+			background-color:#0f1941;
+			-webkit-box-shadow: 0px -10px 30px rgba(0, 0, 0, 0.1);
+			-moz-box-shadow: 0px -10px 30px rgba(0, 0, 0, 0.1);
+			box-shadow: 0px -10px 30px rgba(0, 50, 0, 0.1);
     }
     .copyright{
+			color: white;
       font-family: Helvetica;
-      color: #0f1941;
       font-weight: 700;
       font-size:12pt;
-      padding:10px;
+			padding:10px;
+			margin: auto;
       display: flex;
       align-items: center;
       flex-direction: column;
-			margin: auto;
     }
 </style>
