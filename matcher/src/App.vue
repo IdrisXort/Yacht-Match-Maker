c70c70252c69ae46a338aec964a9790c3080224d
Adjay <adjayramkhelawan@hotmail.com>
2019-01-21 12:08:59 +0100

    So far so good

--- matcher/src/App.vue
+++ matcher/src/App.vue
@@ -17,6 +17,7 @@
     <werk v-show="currentPage==3" :skills="skills" :locations="locations"/>
     <match-page v-show="currentPage==4" :results="[...results]"/>
     <result-page v-show="currentPage==5" :results="[...results]"/>
+    <ResultProfilePage v-show="currentPage==6" />
     <start-button text="start" v-show="currentPage==0" :onClick="goToNextPage"/>
     <previous-button text="previous" v-if="currentPage>1 && currentPage<5" :onClick="goToPreviousPage"/>
     <next-button
@@ -40,6 +41,7 @@ import LeerStijlePage from "./Pages/LeerStijlPage/LeerStijlPage";
 import dataToCompare from "./dataToCompare";
 import ResultPage from "./Pages/ResultPage/ResultPage";
 import MatchPage from "./Pages/MatchPage/MatchPage";
+import ResultProfilePage from "./Pages/ResultProfilePage/ResultProfilePage"
 
 export default {
   components: {
@@ -54,13 +56,14 @@ export default {
     LeerStijlePage: LeerStijlePage,
     matchButton: Button,
     ResultPage: ResultPage,
-    MatchPage: MatchPage
+    MatchPage: MatchPage,
+    ResultProfilePage: ResultProfilePage,
   },
   name: "app",
   data() {
     return {
       currentPage: 0,
-      pageNumbers: [1, 2, 3, 4, 5],
+      pageNumbers: [1, 2, 3, 4, 5, 6],
       questions: data.questions,
       hobbies: data.hobbies,
       icons: data.hobbyIcons,
@@ -73,7 +76,7 @@ export default {
           name: "",
           image: null,
           hobbies: [],
-          info: ""
+          info: ""  
         },
         processiveData: {
           softSkills: [],
---
+++ matcher/src/Pages/ResultProfilePage/ResultProfilePage.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="row">
+    <div class="col-sm-8 profile-container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="profile--picture-container">
+            <div class="profile--picture">
+              <div class="profile--label">Profile picture</div>
+            </div>
+            <p>Name:</p>
+            <p>Emailadres:</p>
+          </div>
+        </div>
+        <div class="col-sm-8 profile--details-container">
+          <p class="profile--oneliner">
+            Ik ben een leergierig en harde werker die elke
+            dag iets nieuws wil leren.
+          </p>
+          <div class="profile--leerstijl-container">
+            <div class="row">
+              <div class="col-sm-3">
+                <b>Denker</b>
+                <img src="../../assets/images/denker-icon.png" alt>
+              </div>
+              <div class="col-sm-9 no-padding-left profile--leerstijl-details">
+                <p>
+                  De kacht van de denker ligt in zijn analytische vermogen. Hij is gericht op informatie,
+                  feiten en begrippen die nodig zijn om een onderwerp conceptueel te begrijpen.
+                  Daarbij zoeken naar logische samenhang en een intellectuele uitdaging.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="profile--hobbies-container">
+            <b>Hobbies</b>
+          </div>
+          <div class="profile-companies-container"></div>
+        </div>
+      </div>
+      <p>
+        <b>Op zoek naar type bedrijf:</b>
+      </p>
+      <div class="row">
+        <Match/>
+        <Match/>
+        <Match/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Match from "../../Components/Match/Match";
+export default {
+  components: {
+    Match: Match
+  }
+};
+</script>
+<style>
+.profile--picture-container {
+  margin: 10px;
+}
+
+.profile-container {
+  border: 1.5px solid #87a0e6;
+  border-radius: 8px;
+  padding: 15px;
+}
+.profile--picture {
+  margin-bottom: 20px;
+  border: 1.5px solid #87a0e6;
+  border-radius: 8px;
+  height: 230px;
+  background-image: url("../../assets/images/sample-image.jpg");
+  background-position: center;
+  background-size: cover;
+}
+
+.profile--label {
+  background-color: #325d835e;
+  color: white;
+  position: relative;
+  top: 75%;
+  transform: translateY(-50%);
+  text-align: center;
+}
+
+.profile--details-container {
+  padding: 10px;
+  
+}
+
+.profile--leerstijl-container {
+    margin-bottom: 20px;
+
+}
+
+.profile--leerstijl-details {
+  position: relative;
+}
+
+.profile--leerstijl-details p {
+  position: absolute;
+  bottom: 0px;
+  font-size: 12px;
+  margin-bottom: 0px;
+  width: 90%;
+}
+</style>
